e2cd2b38439d0a43186c5ee7d56083889b33846a
unknown <freitascontato2004@gmail.com>
2022-09-25 17:52:50 -0300

    Resolução do exercício Heron

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double Lado1, Lado2, Lado3;
+double p; // Semiperimetro
+double Area;
+
+Console.WriteLine("Digite os lados do triângulo desejado.\n");
+
+Console.Write("Lado 1..: ");
+Lado1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Lado 2..: ");
+Lado2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Lado 3..: ");
+Lado3 = Convert.ToDouble(Console.ReadLine());
+
+p = (Lado1 + Lado2 + Lado3) / 2;
+
+Area = Math.Sqrt(p * (p - Lado1) * (p - Lado2) * (p - Lado3));
+
+Console.WriteLine($"Semiperimetro..: {p}");
+Console.WriteLine($"Area...........: {Area}");
